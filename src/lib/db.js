894709a69952c55910362f2137fd892ec475b3db
Alex Passos <alexdesouzapassos@gmail.com>
2026-09-05 11:21:06 -0300

    Commit Inicial

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,1 +1,7 @@
-npm install @neondatabase/serverless
+import { neon } from '@neondatabase/serverless';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL não configurada.');
+}
+
+export const sql = neon(process.env.DATABASE_URL);
